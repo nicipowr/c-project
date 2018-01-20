a45792c9ed23426dbe3a085081919975119f1520
nicipowr <nicola.abgottspon@gmail.com>
2018-01-20 16:36:41 +0100

    niciversion

--- ps.c
+++ ps.c
@@ -9,29 +9,12 @@
 #include <string.h>
 #include <ctype.h>
 
-char *trimwhitespace(char *str)
-{
-  char *end;
-
-  while(isspace((unsigned char)*str)) str++;
-
-  if(*str == 0)  // All spaces?
-    return str;
-
-  end = str + strlen(str) - 1;
-  while(end > str && isspace((unsigned char)*end)) end--;
-
-  *(end+1) = 0;
-
-  return str;
-}
-
 int main(void) {
   DIR *dp;
   FILE *fp;
   struct dirent *ep;
   struct stat statbuf;
-  char *filepathtostatus;
+  char *filepathtostat;
 
   if ((dp = opendir("/proc")) != NULL) {
     bool numbers_only(char charac[]) {
@@ -46,57 +29,25 @@ int main(void) {
     puts("PID Name");
     
     char name[10];
-    // unsigned uid1 = 0, uid2 = 0, uid3 = 0, uid4 = 0;
+    long int rss;
     
     while (ep = readdir(dp)) {
       //stat(ep->d_name,&statbuf);
       if (numbers_only(ep->d_name)) {
-	char *filepathtostatus = malloc(30);
-	strcat(filepathtostatus, "/proc/");
-	strcat(filepathtostatus, ep->d_name);
-	strcat(filepathtostatus, "/status");
+	char *filepathtostat = malloc(30);
+	strcat(filepathtostat, "/proc/");
+	strcat(filepathtostat, ep->d_name);
+	strcat(filepathtostat, "/stat");
 
-	if ((fp = fopen(filepathtostatus,"r")) != NULL) {
-	  int vmrss = 0;
-
-	  fscanf(fp, "Name:\t%s", name);
-
-	  // fscanf(fp, "Uid:\t%u\t%u\t%u\t%u", &uid1, &uid2, &uid3, &uid4);
-	  //*str += 5;
-
-	  // if (uid1 != 0 && uid2 != 0 && uid3 != 0 && uid4 != 0)
-	  printf("%s %s\n", ep->d_name, name);
-	  //printf("%s ", ep->d_name);
-	  //printf("%s \n",name);
-	      
-	  /*
-	    if (strstr(str,"VmRSS:")) {
-	    char *p = str;
-	    while (*p) { // While there are more characters to process...
-	    if (isdigit(*p)) { // Upon finding a digit, ...
-	    long val = strtol(p, &p, 10); // Read a number, ...
-	    vmrss = val;
-	    } else { // Otherwise, move on to the next character.
-	    p++;
-	    }
-	    }
-	    //char *foo = trimwhitespace(str);
-	    //foo += 7;
-	    //strcat()
-	    printf("%d\n",vmrss);
-	    }
-	  */
-	  //if (strstr(str,"Uid:")){
-
-	  //  if (getuid() == )
-
-	  //}
-
-	  //if (statbuf.st_uid == getuid()) {
+	if ((fp = fopen(filepathtostat,"r")) != NULL) {
+	  fscanf(fp, "%*d %s %*c %*d %*d %*d %*d %*d %*u %*u %*u %*u %*u %*u %*u %*d %*d %*d %*d %*d %*d %*u %*u %ld", name, &rss);
+	  // name[sizeof(name) - 1] = "\0";
+	  // strncpy(name, name, sizeof(name) - 1);
+	  printf("%s %s %ld\n", ep->d_name, name, rss);
 	} else
 	  printf("Couldn't open the file.");
 	fclose(fp);	
-	free(filepathtostatus);
+	free(filepathtostat);
       }
     }
     closedir(dp);
