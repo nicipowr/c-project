164bb26c7189a3a0ae00241ee140cf8fc5fb6732
nicipowr <nicola.abgottspon@gmail.com>
2018-01-20 18:51:37 +0100

    try to fix RSS

--- ps.c
+++ ps.c
@@ -25,11 +25,10 @@ char *trimwhitespace(char *str) {
   return str;
 }
 
-char *getStatusFilePath(char *name) {
+char *getPathToPID(char *name) {
   char *filePath = malloc(12);
   strcpy(filePath, "/proc/");
   strcat(filePath, name);
-  strcat(filePath, "/status");
   return filePath;
 }
 
@@ -52,6 +51,8 @@ int main(void) {
 
     puts("PID\tCommand\tRSS");
 
+    char *pathToStat;
+    char *pathToStatus;
     FILE *fp;
     int ret;
     long int rss = 0;
@@ -59,8 +60,17 @@ int main(void) {
     while (ep = readdir(dp)) {
       //stat(ep->d_name,&statbuf);
       if (numbers_only(ep->d_name)) {
-	char *path = getStatusFilePath(ep->d_name);
-	fp = fopen(path, "r");
+	char *pathToPID = getPathToPID(ep->d_name);
+
+	pathToStat = pathToPID;
+	strcat(pathToStat, "/stat");
+	fp = fopen(pathToStat, "r");
+	ret = fscanf(fp, "%*d %*s %*c %*d %*d %*d %*d %*d %*u %*u %*u %*u %*u %*u %*u %*d %*d %*d %*d %*d %*d %*u %*u %ld", &rss);
+	fclose(fp);
+
+	pathToStatus = pathToPID;
+	strcat(pathToStatus, "/status");
+	fp = fopen(pathToStatus, "r");
 	free(path);
 
 	char str[250];
@@ -101,9 +111,7 @@ int main(void) {
 	    if (strcmp(finalstruid,tmpuid)==0) {
 	      printf("%s\t", ep->d_name);
 	      printf("%s\t", name);
-			    
-	      // printf("%ld\n", rss);
-	      ret = fscanf(fp, "%*d %*s %*c %*d %*d %*d %*d %*d %*u %*u %*u %*u %*u %*u %*u %*d %*d %*d %*d %*d %*d %*u %*u %ld", &rss);
+
 	      printf("%d\n", ret);
 	      // name[sizeof(name) - 1] = "\0";
 	      // strncpy(name, name, sizeof(name) - 1);
